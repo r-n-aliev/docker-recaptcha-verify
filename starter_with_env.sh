419f7759ec720f345bc3726b24c2eeb7f2f15e0a
r-n-aliev <r.n.o.aliev@gmail.com>
2019-06-18 19:38:28 +0300

    Update starter_with_env.sh

--- starter_with_env.sh
+++ starter_with_env.sh
@@ -1,7 +1,7 @@
 export EXT_PORT=8080
 export PORT=8000
 export CAPTCHA_SITEVERIFY_URI=https://www.google.com/recaptcha/api/siteverify
-export SECRET_KEY=6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe.    # only for testing purposes
+export SECRET_KEY=6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe    # only for testing purposes
 
 echo "Environment variables was prepared."
 docker-compose up -d
