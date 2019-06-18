f5daa9d4f1cf5fc0b1f4cc1e3084d15379f6be89
r-n-aliev <r.n.o.aliev@gmail.com>
2019-06-18 18:34:39 +0300

    Update starter_with_env.sh

--- starter_with_env.sh
+++ starter_with_env.sh
@@ -3,5 +3,5 @@ export PORT=8000
 export CAPTCHA_SITEVERIFY_URI=https://www.google.com/recaptcha/api/siteverify
 export SECRET_KEY=6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe
 
-echo "Enviroment variables was prepared."
+echo "Environment variables was prepared."
 docker-compose up -d
